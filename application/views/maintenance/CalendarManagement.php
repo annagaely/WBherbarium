e8de1a1578fad4056dadb3d2f822280c9a57c5e0
annagaely <bannagabriella@gmail.com>
2018-08-08 19:58:04 +0800

    yes

--- application/views/maintenance/CalendarManagement.php
+++ application/views/maintenance/CalendarManagement.php
@@ -1,1 +1,1 @@
-<iframe src="<?php echo site_url('admin/view_calendar');?>" width="100%" height="575px"></iframe>
+<iframe src="<?php echo site_url('admin/view_calendar');?>" width="100%" height="570px"></iframe>
