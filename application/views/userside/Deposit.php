28c91f7f0da9024512f70192ae04d3913b291aaf
annagaely <bannagabriella@gmail.com>
2018-08-18 22:51:38 +0800

    idk

--- application/views/userside/Deposit.php
+++ application/views/userside/Deposit.php
@@ -1,46 +1,34 @@
 <img src="<?php echo base_url()?>assets/bower_components/mdbootstrap/img/loan.jpg" style="height: 50vh; width: 100%;"  class="banner">
 <div class="py-5">
-  <div class="card mx-auto px-4" style="width: 80%;">
+  <div class="card mx-auto px-4" style="width: 50%;">
     <div class="card-body">
       <form>
         <p class="card-header white-text text-center py-4 h4" style="background-color: #800000;">Deposit</p>
         <div class="md-form">
-          <div class="row">
-            <div class="col-md-6">
-
-				    </div>
-          </div>
-          <div class="col-md-6">
-            <div class="md-form">
-              <label>Upload Image here:</label>
-              <input type="file" name="imgFeaturedPlant" id = "imgFP" onchange="readURL(this);">
-            </div>
-          </div>
+          <input type="file" class="font-weight-light">
+        </div>
+        <div class="md-form">
+          <input type="text" id="strCName" class="form-control">
+          <label for="strCName" class="font-weight-light">Collector's Name</label>
+        </div>
+        <div class="md-form">
+          <input type="text" id="strLocation" class="form-control">
+          <label for="strLocation" class="font-weight-light">Location</label>
+        </div>
+        <div class="md-form">
+            <input type="date" name="dateLoan" class="form-control grey-text font-weight-light" value="">
+        </div>
+        <div class="md-form">
+          <i class="fas fa-pencil-alt prefix grey-text"></i>
+          <textarea type="text" id="strPlantDesc" class="md-textarea form-control" rows="3"></textarea>
+          <label for="strPlantDesc" class="font-weight-light">Plant Description</label>
         </div>
         <div class="text-center py-4 mt-3">
-          <button class="btn btn-danger" type="submit">Register</button>
+          <button class="btn btn-danger" type="reset">Reset</button>
+          <button class="btn btn-primary" type="submit">Deposit</button>
         </div>
       </form>
     </div>
   </div>
 </div>
-
-  <script type="text/javascript">
-      function readURL(input) {
-              if (input.files && input.files[0]) {
-                  var reader = new FileReader();
-
-                  reader.onload = function (e) {
-                      $('#FPimg')
-                          .attr('src', e.target.result)
-                          .width('200px')
-                          .height('200px');
-                  };
-                  reader.readAsDataURL(input.files[0]);
-              }
-          }
-  $("imgFP").change(function()
-  {
-  	readURL(this);
-  });
-        </script>
+\
