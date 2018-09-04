e3fcf15d1aac7ef31ab97e8e7c7812fba3314294
annagaely <bannagabriella@gmail.com>
2018-09-04 11:49:35 +0800

    profile

---
+++ application/views/userside/MyProfile.php
@@ -0,0 +1,14 @@
+<ul class="nav md-pills nav-justified">
+    <li class="nav-item">
+        <a class="nav-link active" href="#!">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#!">Link 1</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#!">Link 2</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#!">Link 3</a>
+    </li>
+</ul>
