e3bfdc7ab48c55b12480cefadafdef35cba11ed4
annagaely <bannagabriella@gmail.com>
2018-10-05 03:42:50 +0800

    search results

--- application/views/userside/Collection.php
+++ application/views/userside/Collection.php
@@ -1,5 +1,4 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+
 <!--Carousel Wrapper-->
     <div id="carousel-example-2" class="carousel slide carousel-fadev h-100" data-ride="carousel">
         <!--Indicators-->
@@ -133,7 +132,7 @@
                 <h5 class="h5-responsive">Enter rank above the level of Genus:</h5>
                 <form class="form-inline">
                   <input class="form-control form-control-sm w-75" type="text" placeholder="e.g. Phylum, Class, Order, Family" aria-label="Search">
-                  <button type="button" class="btn btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
+                  <button onclick="showTableRank()" type="button" class="btn btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
                 </form>
               </div>
             </div>
@@ -142,7 +141,7 @@
                 <h5 class="h5-responsive">Enter a Genus Name:</h5>
                 <form class="form-inline">
                   <input class="form-control form-control-sm w-75" type="text" placeholder="e.g. Acacia, Pinus" title="Refers to general type of plants" aria-label="Search">
-                  <button type="button" class="btn btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
+                  <button onclick="showTableGenus()" type="button" class="btn btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
                 </form>
               </div>
             </div>
@@ -151,7 +150,7 @@
                 <h5 class="h5-responsive">Enter a Specie Name:</h5>
                 <form class="form-inline">
                   <input class="form-control form-control-sm w-75" type="text" placeholder="e.g. Indicus, Purpurea" title="Refers to a group of closely related plants" aria-label="Search">
-                  <button type="button" class="btn btnsearch btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
+                  <button onclick="showTableSpecie()" type="button" class="btn btnsearch btn-outline-primary waves-effect" style="padding-top:1%; padding-bottom: 1%;padding-left: 2%; padding-right:2%; border: 2px solid #800000!important; color: #800000!important; font-size: 11px!important;">Search</button>
                 </form>
               </div>
             </div>
@@ -163,149 +162,137 @@
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-12">
-
-      <div id="mdb-lightbox-ui"></div>
-
-      <div class="mdb-lightbox no-margin">
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(121).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(121).jpg"
-              class="img-fluid">
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(96).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(96).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(104).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(104).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(45).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(122).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(122).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(125).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(125).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
-        <figure class="col-md-4">
-          <a class="black-text" href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(119).jpg"
-            data-size="1600x1067">
-            <img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(119).jpg"
-              class="img-fluid" />
-            <h3 class="text-center my-3">Photo title</h3>
-          </a>
-        </figure>
-
+  <!-- TABLE RANK RESULTS -->
+  <center>
+    <div style="padding-left: 200px; padding-right: 200px">
+      <div class="table-responsive text-nowrap">
+        <table class="table table-hover table-bordered" id="searchRankTable" style=" display: none;">
+          <thead>
+            <tr>
+              <th scope="col">Rank</th>
+              <th scope="col">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Jacob</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+    </div>
+  </center>
 
+  <!--TABLE GENUS RESULTS-->
+  <center>
+    <div style="padding-left: 200px; padding-right: 200px">
+      <div class="table-responsive text-nowrap">
+        <table class="table table-hover table-bordered" id="searchGenusTable" style="display: none;">
+          <thead>
+            <tr>
+              <th scope="col">Genus</th>
+              <th scope="col">Phylum</th>
+              <th scope="col">Family</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Jacob</td>
+              <td>Jacob</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </center>
+
+  <!--TABLE SPECIE RESULTS-->
+  <center>
+    <div style="padding-left: 350px; padding-right: 350px">
+      <div class="table-responsive text-nowrap">
+        <table class="table table-hover table-bordered" id="searchSpecieTable" style="display: none;">
+          <thead>
+            <tr>
+              <th scope="col">Specie Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a data-toggle="modal" data-target="#herbariumSheet">Jacob</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </center>
+  <!-- Central Modal Small -->
+<div class="modal fade" id="herbariumSheet" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <!-- Change class .modal-sm to change the size of the modal -->
+    <div class="modal-dialog modal-fluid" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body px-5 mx-5">
+          <div class="row mx-5">
+            <div class="col-md-4">
+              <div class="card">
+                img of herbarium sheet
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card">
+                contents
+              </div>
+            </div>
+          </div>
+          <section class="gallery-block grid-gallery">
+              <div class="container">
+                  <div class="row px-5">
+                      <div class="col-md-6 col-lg-4 item">
+                          <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                              <img class="img-fluid image scale-on-hover" src="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                          </a>
+                      </div>
+                      <div class="col-md-6 col-lg-4 item">
+                          <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                              <img class="img-fluid image scale-on-hover" src="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                          </a>
+                      </div>
+                      <div class="col-md-36 col-lg-4 item">
+                          <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                              <img class="img-fluid image scale-on-hover" src="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </section>
+        </div>
+      </div>
     </div>
   </div>
-  <section class="gallery-block grid-gallery">
-      <div class="container">
-          <div class="heading">
-              <h2>Grid Gallery</h2>
-          </div>
-          <div class="row">
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image1.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image1.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image2.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image2.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image3.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image3.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image4.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image4.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image5.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image5.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image6.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image6.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image7.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image7.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image8.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image8.jpg">
-                  </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item">
-                  <a class="lightbox" href="<?php echo base_url()?>assets/bower_components/gallery/img/image9.jpg">
-                      <img class="img-fluid image scale-on-hover" src="../img/image9.jpg">
-                  </a>
-              </div>
-          </div>
-      </div>
-  </section>
+  <!-- Central Modal Small -->
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+  <script>
+      baguetteBox.run('.grid-gallery', { animation: 'slideIn'});
+  </script>
+  <script>
+    function showTableRank() {
+      var ranktbl = document.getElementById("searchRankTable");
+      ranktbl.style.display = "table";
+    }
+    function showTableGenus() {
+      var genustbl = document.getElementById("searchGenusTable");
+      genustbl.style.display = "table";
+    }
+    function showTableSpecie() {
+      var specietbl = document.getElementById("searchSpecieTable");
+      specietbl.style.display = "table";
+    }
+  </script>
