116de12ae4991d2da9947cce63ae3731dc6c09a9
annagaely <bannagabriella@gmail.com>
2018-10-07 22:25:21 +0800

    gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 
 	//LAPTOP-0L08K6U2
-	'hostname' => 'DEI',
+	'hostname' => 'LAPTOP-0L08K6U2',
 	'username' => 'sa',
 	'password' => '1234',
 	'database' => 'HerbariumDatabaseBackup',
